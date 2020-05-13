f8b2468b9411cc516352ecd0ea8613eec5b3750b
IkumaOka <ikuma1018soccer06@gmail.com>
2020-05-13 11:24:57 +0900

    skill_up.cpp

--- beginner167/skill_up.cpp
+++ beginner167/skill_up.cpp
@@ -3,13 +3,5 @@
 using namespace std;
 
 int main() {
-  int N, M, X;
-  // cin >> N >> M >> X;
-  vector<vector<int> > data(N, vector<int>(M));
-  // for (int i = 0; i < N; i++) {
-  //     for (int j = 0; j < (M+1); j++) {
-  //       cin >> data.at(i).at(j);
-  //     }
-  //   }
-    cout << data << endl;
+  int a[12][13];
 }
