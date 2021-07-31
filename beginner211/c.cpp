2166d8dec6ed3fc87cb97e403309030632033100
Ikuma Oka <ikuma1018soccer06@gmail.com>
2021-07-31 12:47:39 +0900

    add beginner211-C

--- beginner211/c.cpp
+++ beginner211/c.cpp
@@ -4,19 +4,22 @@ using namespace std;
 using ll = long long;
 using P = pair<int, int>;
 
-int main() { 
+int main() {
   string s;
   cin >> s;
   int n = s.size();
-  vector dp(n+1, vector<int>(9));
+  vector<vector<int>> dp(n+1 , vector<int>(9 , 0));
   rep(i, n+1) dp[i][0] = 1;
-  const int mod = 1000000007;
   string t = "chokudai";
-  rep(i, n)rep(j, 8){
-    if(s[i] != t[j]){
-      dp[i+1][j+1] = dp[i][j+1];
-    } else {
-      dp[i+1][j+1] = (dp[i][j+1] + dp[i][j]) % mod;
+  int mod = 1000000007;
+  rep(i, n){
+    rep(j, 8){
+      if (t[j] != s[i]){
+        dp[i+1][j+1] = dp[i][j+1];
+      }
+      else {
+        dp[i+1][j+1] = (dp[i][j+1] + dp[i][j]) % mod;
+      }
     }
   }
   cout << dp[n][8] << endl;
