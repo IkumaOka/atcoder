4854a8495af734e63c6180bf6747ef6a4f6396e1
Ikuma Oka <ikuma1018soccer06@gmail.com>
2021-08-02 10:35:16 +0900

    modify c_ans

--- beginner212/c_ans.cpp
+++ beginner212/c_ans.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 #define rep(i,n) for (int i = 0;i < (n); ++i)
-#define INF 10000000;
+#define INF 1001001001;
 using namespace std;
 using ll = long long;
 using P = pair<int, int>;
